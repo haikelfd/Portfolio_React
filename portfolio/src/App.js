9aee5c9eeaac1f91a52f48fd06fde4614eaee122
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2021-07-28 11:12:00 +0100

    About us section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import AboutUs from './pages/AboutUs';
 
 
 function App(){
     return(
         <div className = "App"> 
-        <h1>Film</h1>
-        </div>
+        <AboutUs/>
+         </div>
     );
 }
 
