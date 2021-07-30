b61b2c782af457948dbacabca6258af24a9877ff
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2021-07-30 20:53:04 +0100

    Adding the FAQ Section
    Adding 4 frequently asked questions

---
+++ portfolio/src/components/FaqSection.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+const FaqSection =() => {
+    return (
+        <div className="faq">
+            <h2> Any questions<span> FAQ</span></h2>
+            <div className="question">
+            <h4> How Do I Start?</h4>
+            <div className ="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam deleniti, impedit illum eos of</p>
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam </p>
+        </div>
+        </div>
+        <div className="question">
+            <h4> Daily schedule</h4>
+            <div className ="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam deleniti, impedit illum eos of</p>
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam </p>
+        </div>
+        </div>   <div className="question">
+            <h4> Different Payment Methods</h4>
+            <div className ="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam deleniti, impedit illum eos of</p>
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam </p>
+        </div>
+        </div>   <div className="question">
+            <h4> What product do you offre</h4>
+            <div className ="answer">
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam deleniti, impedit illum eos of</p>
+            <p>Lorem ipsum, dolor sit amet consectetur aut suscipit dignissimos omnis totam </p>
+        </div>
+        </div>
+        </div>
+
+    );
+};
+export default FaqSection ;
