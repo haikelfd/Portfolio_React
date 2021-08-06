085dec51d06a77a422a357c094a2d3e12e431416
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2021-08-06 23:29:50 +0100

    test

--- portfolio/src/pages/AboutUs.js
+++ portfolio/src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import FaqSection from '../components/FaqSection';
+import nav from "./ContactUs";
 // Animation 
 import { motion } from 'framer-motion';
 import {pageAnimation} from  "../animation";
