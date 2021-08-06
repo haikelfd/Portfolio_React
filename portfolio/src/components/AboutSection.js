c7467126db49387bc14410515a3f3056480b2cc9
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2021-08-07 00:19:58 +0100

    added animation in the About us Section

--- portfolio/src/components/AboutSection.js
+++ portfolio/src/components/AboutSection.js
@@ -6,6 +6,7 @@ import { About,Hide,Description,Image } from '../styles';
 // Framer Motion
 
 import {motion} from "framer-motion";
+import { titleAnim,fade,photoAnim } from '../animation';
 
 
 
@@ -14,22 +15,22 @@ const AboutSection = () =>{
    
     <About>
         <Description>
-            <div className="title">
+            <motion.div>
                 <Hide>
-                    <motion.h2> We work  to make</motion.h2>
+                    <motion.h2 variants= {titleAnim}> We work  to make</motion.h2>
                 </Hide>
                 <Hide>
-                    <h2> your <span>dreams</span>  come</h2>
+                    <motion.h2 variants= {titleAnim}> your <span>dreams</span>  come</motion.h2>
                 </Hide>
                 <Hide>
-                    <h2>true.</h2>
+                    <motion.h2 variants= {titleAnim}>true.</motion.h2>
                 </Hide>
-            </div>
-            <p> Contact us  for any photography or videography ideas that you have. We have professionals with amazing skills</p>
-            <button>Contact Us</button>
+            </motion.div>
+            <motion.p variants={fade}> Contact us  for any photography or videography ideas that you have. We have professionals with amazing skills</motion.p>
+            <motion.button>Contact Us</motion.button>
         </Description>
         <Image>
-            <img src={home1} alt="guy with a camera"/>
+            <motion.img  variants={photoAnim}src={home1} alt="guy with a camera"/>
         </Image>
     </About>
 
